19df4ed9abb7d6f8ef9df61a3e37517857f6cb54
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-13 15:38:09 -0800

    Use FutureBuilder for waiting for firebase to initialize

---
+++ lib/loading.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  final String title;
+
+  const LoadingPage({this.title = '', Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text(title, style: TextStyle(color: Colors.pink)));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,41 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'loading.dart';
 import 'login.dart';
 
 void main() {
-  Firebase.initializeApp().then((FirebaseApp app) => runApp(MyApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Food Ordering App',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: LoginPage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        Widget w;
+        if (snapshot.hasError) {
+          w = LoadingPage(title: 'Error');
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          w = LoginPage();
+        } else {
+          w = LoadingPage(title: 'Loading');
+        }
+        return MaterialApp(
+          title: 'Food Ordering App',
+          theme: ThemeData(primarySwatch: Colors.deepPurple),
+          home: w,
+        );
+      },
     );
   }
 }
