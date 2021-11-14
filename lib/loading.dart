05e4514689333fc411756dc7a26a41c48b72f684
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-13 23:52:11 -0800

    Require title argument

--- lib/loading.dart
+++ lib/loading.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class LoadingPage extends StatelessWidget {
   final String title;
 
-  const LoadingPage({this.title = '', Key? key}) : super(key: key);
+  const LoadingPage({required this.title, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
