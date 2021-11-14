ff54d6d7701ebd666710fa341d667eeb1d92b2f9
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-13 18:43:56 -0800

    Toggle user data display and login and sign up buttons

--- lib/account.dart
+++ lib/account.dart
@@ -23,42 +23,44 @@ class _AccountPageState extends State<AccountPage> {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Padding(
+          FirebaseAuth.instance.currentUser == null
+              ? Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => SignUpPage(),
-                      ));
-                    },
-                    child: Text('Sign Up'),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => SignUpPage(),
+                            ));
+                          },
+                          child: Text('Sign Up'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ));
+                          },
+                          child: Text('Log In'),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => LoginPage(),
-                      ));
-                    },
-                    child: Text('Log In'),
+                )
+              : ListTile(
+                  title: Text(
+                    'FirstName LastName',
+                    style: Theme.of(context).textTheme.headline5,
                   ),
+                  subtitle:
+                      Text(FirebaseAuth.instance.currentUser?.email ?? ''),
                 ),
-              ],
-            ),
-          ),
-          ListTile(
-            title: Text(
-              'FirstName LastName',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            subtitle: Text(FirebaseAuth.instance.currentUser?.email ?? ''),
-          ),
           ListTile(
             title: Text('Personal Info'),
             trailing: Icon(Icons.navigate_next_rounded),
