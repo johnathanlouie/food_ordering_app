79c9ecce60b01c386474878324a1e4f13cf4cb97
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-25 15:28:07 -0800

    Create menu_item.dart

---
+++ lib/menu_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuItem extends StatefulWidget {
+  const MenuItem({Key? key}) : super(key: key);
+
+  @override
+  _MenuItemState createState() => _MenuItemState();
+}
+
+class _MenuItemState extends State<MenuItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
