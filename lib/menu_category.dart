170bd8899255c17926961aefe4b7fb6cdb837d9e
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-25 22:32:24 -0800

    Make menu item type a field

--- lib/menu_category.dart
+++ lib/menu_category.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:lwd_food_ordering_app/menu_item.dart';
 
 class MenuCategoryPage extends StatelessWidget {
-  const MenuCategoryPage(MenuItemType menuItemType, {Key? key})
-      : super(key: key);
+  final MenuItemType menuItemType;
+
+  const MenuCategoryPage(this.menuItemType, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
