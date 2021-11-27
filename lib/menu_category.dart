0256f079f120b158fda410e19c72f4d5415c5bd1
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-26 23:18:13 -0800

    Display menu items on menu category screen

--- lib/menu_category.dart
+++ lib/menu_category.dart
@@ -9,6 +9,18 @@ class MenuCategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text(menuItemType.name)),
+      body: ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          leading: Image.asset(menuItems[menuItemType]![index]['image']),
+          title: Text(menuItems[menuItemType]![index]['name']),
+          subtitle: Text(
+            "\$${menuItems[menuItemType]![index]['price']} ${menuItems[menuItemType]![index]['minCalories']}-${menuItems[menuItemType]![index]['maxCalories']} Cal.",
+          ),
+        ),
+        itemCount: menuItems[menuItemType]!.length,
+      ),
+    );
   }
 }
