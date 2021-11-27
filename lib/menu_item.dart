8859549fdd6f99a2a70dfd806612af70bed629c7
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-26 23:12:56 -0800

    Create menu items list

--- lib/menu_item.dart
+++ lib/menu_item.dart
@@ -1,6 +1,240 @@
 import 'package:flutter/material.dart';
 import 'package:lwd_food_ordering_app/menu_types.dart';
 
+final Map<MenuItemType, List<Map<String, dynamic>>> menuItems = const {
+  MenuItemType.mainDish: [
+    {
+      'name': 'Spicy Crispy Chicken Sandwich 0',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich 1',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich 2',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.sideDish: [
+    {
+      'name': 'Crispy Chicken Sandwich 0',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Crispy Chicken Sandwich 1',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Crispy Chicken Sandwich 2',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.snack: [
+    {
+      'name': 'Bacon Quarter Pounder with Cheese 0',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Bacon Quarter Pounder with Cheese 1',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Bacon Quarter Pounder with Cheese 2',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.appetizer: [
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.sweet: [
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.beverage: [
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.deal: [
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+  MenuItemType.limitedTimeOnly: [
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+    {
+      'name': 'Spicy Crispy Chicken Sandwich',
+      'price': 5.99,
+      'image': 'assets/food.png',
+      'description':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'minCalories': 100,
+      'maxCalories': 400,
+    },
+  ],
+};
 
 class MenuItem extends StatefulWidget {
   const MenuItem({Key? key}) : super(key: key);
