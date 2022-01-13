ec7720f18577a8444955a902cc8d14ad4a9b7189
Johnathan Louie <johnathanlouie88@gmail.com>
2022-01-12 22:24:37 -0800

    Create shopping_cart.dart

---
+++ lib/global_states/shopping_cart.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/widgets.dart';
+import 'package:lwd_food_ordering_app/menu_types.dart';
+
+class ShoppingCart extends ChangeNotifier {
+  static double taxRate = 0.095;
+  Map<MenuItemData, int> _items = new Map();
+
+  get items => _items;
+
+  void addItem(MenuItemData item, int count) {
+    if (count < 0) {
+      throw new Exception('Cannot add negative number of items.');
+    }
+    int? oldCount = _items[item];
+    if (oldCount == null) {
+      oldCount = 0;
+    }
+    _items[item] = oldCount + count;
+    notifyListeners();
+  }
+
+  void removeItem(MenuItemData item, int count) {
+    if (count < 0) {
+      throw new Exception('Cannot remove negative number of items.');
+    }
+    int? oldCount = _items[item];
+    if (oldCount == null) {
+      oldCount = 0;
+    }
+    if (oldCount < count) {
+      throw new Exception('Cannot remove more than current number of items.');
+    }
+    _items[item] = oldCount - count;
+    notifyListeners();
+  }
+
+  void removeAll(MenuItemData item) {
+    _items[item] = 0;
+    notifyListeners();
+  }
+
+  double get subtotal {
+    double subtotal = 0.0;
+    for (var e in _items.entries) {
+      subtotal += e.key.price * e.value;
+    }
+    return subtotal;
+  }
+
+  double get tax => subtotal * taxRate;
+
+  double get grandTotal => subtotal + tax;
+}
