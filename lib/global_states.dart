ab82859aafd9bda1096d33a7fb559e9e0eab02fa
Johnathan Louie <johnathanlouie88@gmail.com>
2022-02-06 18:07:20 -0800

    Rename to UserState.

--- lib/global_states.dart
+++ lib/global_states.dart
@@ -1,4 +1,4 @@
 library global_states;
 
 export 'package:lwd_food_ordering_app/global_states/shopping_cart.dart';
-export 'package:lwd_food_ordering_app/global_states/user.dart';
+export 'package:lwd_food_ordering_app/global_states/user_state.dart';
--- lib/global_states/user_state.dart
+++ lib/global_states/user_state.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-class User extends ChangeNotifier {
+class UserState extends ChangeNotifier {
   String _firstName = '';
   String _lastName = '';
   bool _loggedIn = false;
